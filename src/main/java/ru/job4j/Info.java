7e7b55d2895d22ae237cd320381611efe3e6be98
kin23i <kin23i@yandex.ru>
2023-10-21 22:53:33 +0800

    4.2. Заголовок в файле java [#159036 #453896] Обновлен Info.java Убиран комментарий

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -2,6 +2,6 @@ package ru.job4j;
 
 public class Info {
     public static void main(String[] args) {
-        System.out.println("20.10.23"); //обновление 06:37 МСК
+        System.out.println("20.10.23");
     }
 }
