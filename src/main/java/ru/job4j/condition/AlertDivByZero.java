f05f4f6dc9bb1a6a00931b154b4c9636792aa5ec
kin23i <kin23i@yandex.ru>
2023-10-30 12:35:39 +0800

    4.1. Операторы ветвлений [#3611 #456351]

--- src/main/java/ru/job4j/condition/AlertDivByZero.java
+++ src/main/java/ru/job4j/condition/AlertDivByZero.java
@@ -2,12 +2,10 @@ package ru.job4j.condition;
 
 public class AlertDivByZero {
     public static void possibleDiv(int number) {
-        boolean result = number == 0;
-        if (result) {
+        if (number == 0) {
             System.out.println("Could not div by 0.");
         }
-        result = number < 0;
-        if (result) {
+        if (number < 0) {
             System.out.println("This is negative number");
         }
     }
